4639e10192cfa09db6ad0c8e1e33cf61c1863253
Martijn Nieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2021-04-24 18:58:38 +0200

    Add initial setup

---
+++ templates/Default/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss'
+
+export default function DefaultTemplate() {
+  return (
+    <main className={styles['container']}>
+      <h1>Template</h1>
+    </main>
+  )
+}
