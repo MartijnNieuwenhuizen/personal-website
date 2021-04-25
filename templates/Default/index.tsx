2f1d775a092277782092bea51a909c7b786f27d7
Martijn Nieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2021-04-25 12:09:12 +0200

    Add 2 default layouts

--- templates/Default/index.tsx
+++ templates/Default/index.tsx
@@ -1,9 +1,29 @@
+import type { ReactElement } from 'react'
+
 import styles from './styles.module.scss'
 
-export default function DefaultTemplate() {
+interface Props {
+  title: string
+  children: ReactElement
+
+  subtitle?: string
+  intro?: string
+}
+
+export default function DefaultTemplate({
+  children,
+  title,
+  subtitle,
+  intro,
+}: Props) {
   return (
-    <main className={styles['container']}>
-      <h1>Template</h1>
-    </main>
+    <>
+      <header>
+        <h1>{title}</h1>
+        {subtitle && <h2>{subtitle}</h2>}
+        {intro && <p>intro</p>}
+      </header>
+      <main className={styles['container']}>{children}</main>
+    </>
   )
 }
